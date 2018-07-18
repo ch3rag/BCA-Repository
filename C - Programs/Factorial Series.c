a1f1f68ef9416074360acc2a30a8993c4101296a
Bharat Singh Rajput <bharatsr1997@gmail.com>
2018-07-19 01:33:55 +0530

    Create Factorial Series.c

---
+++ C - Programs/Factorial Series.c
@@ -0,0 +1,21 @@
+// Chirag Sing Rajput
+// Factorial Series
+
+#include <stdio.h>
+
+  int main(int argc, char * argv[]) {
+    int n, i, j;
+    long int fact = 1, sum = 0;
+    printf("Enter N :");
+    scanf("%d", & n);
+    for (i = 1; i <= n; i++) {
+      printf("%d! +", i);
+      for (j = 1; j <= i; j++) {
+        fact = fact * j;
+      }
+      sum = fact + sum;
+      fact = 1;
+    }
+    printf(" = %ld", sum);
+    return 0;
+  }
