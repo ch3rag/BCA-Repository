a980f22180696a4b50b9ff1399074ef8027096b2
Bharat Singh Rajput <bharatsr1997@gmail.com>
2018-07-19 02:18:01 +0530

    Update Traid Numbers.c

--- C - Programs/Traid Numbers.c
+++ C - Programs/Traid Numbers.c
@@ -5,40 +5,54 @@
 write a program to print triad numbers. any three numbers will be triad numbers if they satisfy the following conditions.
 1) each number is a three number.
 2) all the digits in the three numbers(total 9 digits) should be different.
-
+3) if a,b,c are triad numbers them, b = a * 2 and c = a * 3
+	e.g:
+	219 438 657
+	267 534 801
 */
 
-#
-include < stdio.h >
+#include <stdio.h>
 
     int main(int argc, char * argv[]) {
-
-        int a, b, c, f = 1, i, d = 0, j, e, g;
-        clrscr();
+    	
+        int triads[3], flag = 1;
         printf("Enter Three Numbers : \n");
-        scanf("%d%d%d", & a, & b, & c);
-        if (a < 100 && a > 999 || b < 100 && b > 999 || c < 100 && c > 999)
-            f = 0;
-        else if (b != (a * 2) || c != (a * 3))
-            f = 0;
-        else
-            for (i = 1; i <= 3; i++) {
-                if (a % 10 == e % 10 || a % 10 == g % 10)
-                    f = 0;
-                else {
-                    e /= 10;
-                    g /= 10;
-                }
-                if (b % 10 == c % 10)
-                    f = 0;
-                else
-                    c /= 10;
-
-            }
-        //printf("\n%d",d);
-        if (f == 0)
+        for(int i = 0 ; i < 3 ; i++) {
+        	scanf("%d", &triads[i]);
+		}
+		
+		if(triads[1] != triads[0] * 2 || triads[2] != triads[0] * 3) {
+			flag = 0;
+		}   
+		if(flag == 1) {
+			for(int i = 0 ; i < 3 ; i++) {
+				if(triads[i] < 100 || triads[i] > 999) {
+					flag = 0;
+					break;
+				}
+			}
+		}
+		
+		if(flag == 1) {
+			
+			int digits[10] = {0};
+			for(int i = 0 ; i < 3 ; i++) {
+				while(triads[i] > 0) {
+					int digit = triads[i] % 10;
+					if(digits[digit] != 0) {
+						flag = 0;
+						break;
+					} else {
+						digits[digit] = 1;
+					}
+					triads[i] /= 10;
+				}
+			}
+		}
+	
+        if (flag == 0)
             printf("Numbers Not Triad!");
         else
             printf("Numbers Are Triad!");
-        getch();
+   		return 0;
     }
