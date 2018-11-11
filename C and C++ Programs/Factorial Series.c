dedfd57795f6612db276dd246b7368ebb5e3bdec
Bharat Singh Rajput <bharatsr1997@gmail.com>
2018-11-12 01:27:36 +0530

    Update Factorial Series.c

--- C and C++ Programs/Factorial Series.c
+++ C and C++ Programs/Factorial Series.c
@@ -3,19 +3,19 @@
 
 #include <stdio.h>
 
-  int main(int argc, char * argv[]) {
-    int n, i, j;
-    long int fact = 1, sum = 0;
-    printf("Enter N :");
-    scanf("%d", & n);
-    for (i = 1; i <= n; i++) {
-      printf("%d! +", i);
-      for (j = 1; j <= i; j++) {
-        fact = fact * j;
-      }
-      sum = fact + sum;
-      fact = 1;
+int main(int argc, char *argv[]) {
+  int n, i, j;
+  long int fact = 1, sum = 0;
+  printf("Enter N :");
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++) {
+    printf("%d! +", i);
+    for (j = 1; j <= i; j++) {
+      fact = fact * j;
     }
-    printf(" = %ld", sum);
-    return 0;
+    sum = fact + sum;
+    fact = 1;
   }
+  printf(" = %ld", sum);
+  return 0;
+}
