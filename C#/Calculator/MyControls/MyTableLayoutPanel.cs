f2abe15c0cbc79209eff6cc058ffc63c83236759
Bharat Singh Rajput <bharatsr1997@gmail.com>
2023-08-09 17:04:43 +0530

    Updates

--- C#/Calculator/MyControls/MyTableLayoutPanel.cs
+++ C#/Calculator/MyControls/MyTableLayoutPanel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+
 namespace MyControls {
     public class MyTableLayoutPanel : TableLayoutPanel {
         private bool isPainted;
